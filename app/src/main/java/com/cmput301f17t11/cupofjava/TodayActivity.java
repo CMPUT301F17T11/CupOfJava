e9d469c3c17f842d3d3b1ed79c733c2714161594
mjenius <jamaledd@ualberta.ca>
2017-10-22 11:15:02 -0600

    Edited UserLoginActivity

--- app/src/main/java/com/cmput301f17t11/cupofjava/TodayActivity.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/TodayActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class TodayActivity extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/cmput301f17t11/cupofjava/UserLoginActivity.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/UserLoginActivity.java
@@ -1,14 +1,48 @@
 package com.cmput301f17t11.cupofjava;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class UserLoginActivity extends Activity {
+import static android.R.attr.name;
+
+public class UserLoginActivity extends AppCompatActivity{
+
+    private EditText username_editText;
+    private EditText password_editText;
+    private Button signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        username_editText = (EditText) findViewById(R.id.email);
+        password_editText = (EditText) findViewById(R.id.password);
+
+        signIn = (Button) findViewById(R.id.username_sign_in_button);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserLoginActivity.this, MainActivity.class);
+            }
+        });
+    }
+
+    public void addUsername(){
+        String username = username_editText.getText().toString();
+
+        if (username.isEmpty()) {
+            username_editText.setError("Enter username!");
+            return;
+        }
+    }
+
+    public void addPassword(){
+        String password = password_editText.getText().toString();
+
     }
 }
