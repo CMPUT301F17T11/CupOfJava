6ebb20380e26894cbedb13029f80fd97cde73b7d
nbhuiyan <nazimudd@ualberta.ca>
2017-10-22 17:21:49 -0600

    added support for followers and following in User class

--- app/src/main/java/com/cmput301f17t11/cupofjava/User.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/User.java
@@ -1,7 +1,12 @@
 package com.cmput301f17t11.cupofjava;
 
+import java.util.ArrayList;
+
 public class User {
     private String username;
+    private ArrayList<String> followingList;
+    private ArrayList<String> followerList;
+    private ArrayList<String> followRequestList;
 
     public User(String name){
         setUsername(name);
@@ -13,5 +18,33 @@ public class User {
 
     public String getUsername(){
         return this.username;
+    }
+
+    public void addFollower(String id){}
+
+    public void addFollowing(String id){}
+
+    public void newFollowRequest(String id){}
+
+    public void acceptFollowRequest(String id){}
+
+    public boolean isFollowing(String id){
+        return true;
+    }
+
+    public boolean isFollower(String id){
+        return true;
+    }
+
+    public ArrayList<String> getFollowRequestList(){
+        return this.followRequestList;
+    }
+
+    public ArrayList<String> getFollowingList(){
+        return this.followingList;
+    }
+
+    public ArrayList<String> getFollowerList(){
+        return this.followerList;
     }
 }
