8a835d4002343eaa19a8f96e326eeebb2fc753ee
mjenius <jamaledd@ualberta.ca>
2017-10-23 13:59:27 -0600

    ViewHabitEventActivity Edit

--- app/src/main/java/com/cmput301f17t11/cupofjava/ViewHabitEventActivity.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/ViewHabitEventActivity.java
@@ -3,8 +3,22 @@ package com.cmput301f17t11.cupofjava;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * This activity shows the event that the user has currently started but not ended.
+ * User can see some details of the event such as the habit attached to it,
+ * the reason for the attached habit, the start date of the associative habit,
+ * the photo attached to the habit event, and progress stats.
+ *
+ * User can top on the habit to swtich to any other habits associated with
+ * the event.
+ */
 public class ViewHabitEventActivity extends Activity {
 
+    /**
+     * Launches Interface displaying the habit events and their
+     * basic details.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
