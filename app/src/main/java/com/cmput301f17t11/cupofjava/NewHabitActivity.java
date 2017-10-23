ed55ae0dff5eacf5091f3f5f1117a980ac7689bb
mjenius <jamaledd@ualberta.ca>
2017-10-23 13:45:51 -0600

    NewHabitActivity Edit

--- app/src/main/java/com/cmput301f17t11/cupofjava/NewHabitActivity.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/NewHabitActivity.java
@@ -3,11 +3,21 @@ package com.cmput301f17t11.cupofjava;
 import android.app.Activity;
 import android.os.Bundle;
 
+
+/**
+ * This activity handles adding and configuring an existent habit.
+ * User must add add a name and reason for the habit.
+ */
 public class NewHabitActivity extends Activity {
 
+    /**
+     * Launches the screen to add/edit a habit.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_habit);
     }
+
 }
