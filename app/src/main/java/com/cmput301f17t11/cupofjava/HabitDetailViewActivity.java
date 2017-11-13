08b87d84ec3bd9336fc62ba2c2f8f0ca56663021
mjenius <jamaledd@ualberta.ca>
2017-11-13 01:43:16 -0700

    Added HabitDetailViewActivity

---
+++ app/src/main/java/com/cmput301f17t11/cupofjava/HabitDetailViewActivity.java
@@ -0,0 +1,13 @@
+package com.cmput301f17t11.cupofjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HabitDetailViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_detail_view);
+    }
+}
