e8d226f80a8f4fc8ba4ae5800099d5bf168ba7d5
Sajjad <30275853+sajjadhaiderrr@users.noreply.github.com>
2017-11-12 20:28:34 -0700

    Bottom action bar functionality working

--- app/src/main/java/com/cmput301f17t11/cupofjava/AllHabitViewActivity.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/AllHabitViewActivity.java
@@ -1,8 +1,11 @@
 package com.cmput301f17t11.cupofjava;
 
 import android.app.Activity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
 public class AllHabitViewActivity extends Activity {
 
@@ -10,6 +13,33 @@ public class AllHabitViewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_habit_view);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_today);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+
+                    case R.id.action_favorites:
+                        Intent intent2 = new Intent(AllHabitViewActivity.this, HabitTimeLineActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.action_schedules:
+                        Intent intent3 = new Intent(AllHabitViewActivity.this, TodayViewActivity.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.action_music:
+                        break;
+                    case R.id.add_habit:
+                        Intent intent4 = new Intent(AllHabitViewActivity.this, NewHabitActivity.class);
+                        startActivity(intent4);
+                        break;
+
+                }
+                return false;
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/cmput301f17t11/cupofjava/HabitTimeLineActivity.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/HabitTimeLineActivity.java
@@ -1,7 +1,11 @@
 package com.cmput301f17t11.cupofjava;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
 public class HabitTimeLineActivity extends Activity {
 
@@ -9,5 +13,30 @@ public class HabitTimeLineActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_time_line);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_today);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+
+                    case R.id.action_favorites:
+                        break;
+                    case R.id.action_schedules:
+                        Intent intent2 = new Intent(HabitTimeLineActivity.this, TodayViewActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.action_music:
+                        Intent intent3 = new Intent(HabitTimeLineActivity.this, AllHabitViewActivity.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.add_habit:
+                        Intent intent4 = new Intent(HabitTimeLineActivity.this, NewHabitActivity.class);
+                        startActivity(intent4);
+                        break;
+
+                }
+                return false;
+            }
+        });
     }
 }
