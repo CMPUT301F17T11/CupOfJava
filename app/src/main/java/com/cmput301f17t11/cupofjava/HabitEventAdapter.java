1087276b4808812d1738d3201ec39b5793889683
Sajjad <30275853+sajjadhaiderrr@users.noreply.github.com>
2017-11-13 13:25:54 -0700

    HabitEventAdapter

---
+++ app/src/main/java/com/cmput301f17t11/cupofjava/HabitEventAdapter.java
@@ -0,0 +1,36 @@
+package com.cmput301f17t11.cupofjava;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sajjad on 2017-11-13.
+ */
+
+public class HabitEventAdapter extends ArrayAdapter<HabitEvent> {
+
+    public HabitEventAdapter(Activity context, ArrayList<HabitEvent> counters) {
+        super(context, 0, counters);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.habit_event_list_item, parent, false);
+        }
+
+        final HabitEvent currentHabitEvent = getItem(position);
+
+        return listItemView;
+
+
+    }
+}
