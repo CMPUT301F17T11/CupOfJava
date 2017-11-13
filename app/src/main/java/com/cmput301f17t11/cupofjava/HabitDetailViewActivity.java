d605287ff7b9f4561f9667d64bac31c35113b95c
mjenius <jamaledd@ualberta.ca>
2017-11-13 01:53:42 -0700

    Implementing HabitDetailViewActivity

--- app/src/main/java/com/cmput301f17t11/cupofjava/HabitDetailViewActivity.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/HabitDetailViewActivity.java
@@ -2,12 +2,22 @@ package com.cmput301f17t11.cupofjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class HabitDetailViewActivity extends AppCompatActivity {
+
+    private TextView habitTitleTextView;
+    private TextView habitReasonTextView;
+    private Calendar habitdateCalendar;
+    private String habitDateString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_detail_view);
+
+
     }
 }
