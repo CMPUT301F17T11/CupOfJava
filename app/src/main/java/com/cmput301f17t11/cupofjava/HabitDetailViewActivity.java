bdbc5c4bf2d6ba397872196fd3fc04565e9546b2
mjenius <jamaledd@ualberta.ca>
2017-11-13 02:18:54 -0700

    Implementing HabitDetailViewActivity

--- app/src/main/java/com/cmput301f17t11/cupofjava/HabitDetailViewActivity.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/HabitDetailViewActivity.java
@@ -1,8 +1,15 @@
 package com.cmput301f17t11.cupofjava;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
 
 import java.util.Calendar;
 
@@ -10,14 +17,44 @@ public class HabitDetailViewActivity extends AppCompatActivity {
 
     private TextView habitTitleTextView;
     private TextView habitReasonTextView;
-    private Calendar habitdateCalendar;
-    private String habitDateString;
+    private String habitDateCalendar;
+    private ListView habitEventList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_detail_view);
 
+        habitTitleTextView = (TextView) findViewById(R.id.title_text_view);
+        habitReasonTextView = (TextView) findViewById(R.id.reason_text_view);
 
+    }
+
+    public void addNewHabitEventButton(View view) {
+        Intent intent = new Intent(HabitDetailViewActivity.this, NewHabitEventActivity.class);
+        startActivity(intent);
+    }
+
+    public void deleteCurrentHabitButton(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(HabitDetailViewActivity.this);
+        builder.setTitle("Edit record")
+                .setNegativeButton("DELETE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(HabitDetailViewActivity.this, TodayViewActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setPositiveButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
