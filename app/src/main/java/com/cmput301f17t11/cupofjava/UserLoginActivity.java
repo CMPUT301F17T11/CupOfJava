0690e8f614474bdce9c1a9254c0102febfba799e
gsgould <gsgould@ualberta.ca>
2017-11-29 20:27:42 -0700

    Added cliche quote

--- app/src/main/java/com/cmput301f17t11/cupofjava/UserLoginActivity.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/UserLoginActivity.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -94,4 +95,5 @@ public class UserLoginActivity extends Activity {
             }
         });
     }
+
 }
