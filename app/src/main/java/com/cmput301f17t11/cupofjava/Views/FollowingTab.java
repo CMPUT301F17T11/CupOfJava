a7b96578406d442d9922c3b2ce80a32664a62b6c
nbhuiyan <nazimudd@ualberta.ca>
2017-12-03 00:27:42 -0700

    created activity that will allow searching

--- app/src/main/java/com/cmput301f17t11/cupofjava/Views/FollowingTab.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/Views/FollowingTab.java
@@ -1,13 +1,21 @@
 package com.cmput301f17t11.cupofjava.Views;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.cmput301f17t11.cupofjava.Controllers.ElasticsearchController;
+import com.cmput301f17t11.cupofjava.Models.HabitEvent;
 import com.cmput301f17t11.cupofjava.R;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,43 +23,14 @@ import com.cmput301f17t11.cupofjava.R;
  * Activities that contain this fragment must implement the
  * {@link FollowingTab.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link FollowingTab#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class FollowingTab extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private String userName;
+    //Button addButton;
 
     private OnFragmentInteractionListener mListener;
 
-    public FollowingTab() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FollowingTab.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FollowingTab newInstance(String param1, String param2) {
-        FollowingTab fragment = new FollowingTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    public FollowingTab() {}
 
     /**
      * The system calls this when creating this fragment.
@@ -61,10 +40,9 @@ public class FollowingTab extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
             this.userName = getArguments().getString("userName");
         }
+
     }
 
     /**
@@ -77,7 +55,6 @@ public class FollowingTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_following_tab, container, false);
     }
 
@@ -88,36 +65,16 @@ public class FollowingTab extends Fragment {
         }
     }
 
-    // PLEASE LEAVE THIS
-    /*@Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }*/
-
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    public void addUserButton(View view){}
 }
---
+++ app/src/main/java/com/cmput301f17t11/cupofjava/Views/SearchUserToFolowActivity.java
@@ -0,0 +1,15 @@
+package com.cmput301f17t11.cupofjava.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cmput301f17t11.cupofjava.R;
+
+public class SearchUserToFolowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_user_to_folow);
+    }
+}
