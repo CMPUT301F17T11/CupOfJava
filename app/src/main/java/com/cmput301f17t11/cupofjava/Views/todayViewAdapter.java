b6d2910a0aac8083f72bab196df74a2c4ba89452
gsgould <gsgould@ualberta.ca>
2017-12-03 23:08:52 -0700

    Final UI changes TODAY HABIT ADAPTER

--- app/src/main/java/com/cmput301f17t11/cupofjava/Views/todayViewAdapter.java
+++ app/src/main/java/com/cmput301f17t11/cupofjava/Views/todayViewAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cmput301f17t11.cupofjava.Models.Habit;
