6a02a5db1cb0f28d438bc1d4816077258d1f4891
nbhuiyan <nazimudd@ualberta.ca>
2017-12-03 17:37:17 -0700

    fixed following profile view UI

---
+++ app/src/main/java/com/cmput301f17t11/cupofjava/Views/ViewFollowingProfileActivity.java
@@ -0,0 +1,15 @@
+package com.cmput301f17t11.cupofjava.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cmput301f17t11.cupofjava.R;
+
+public class ViewFollowingProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_following_profile);
+    }
+}
